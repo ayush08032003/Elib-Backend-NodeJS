cfdec9919d9642b41b103d583e123bcda9a6ef54
AyushKumar <ayushkumar08032003@gmail.com>
2024-12-17 17:14:58 +0530

    Add Database Setup

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,7 @@ import { config as conf } from "dotenv";
 conf();
 const _config = {
   port: process.env.PORT || 3000,
+  databaseUrl: process.env.DATABASE_URL || "",
 };
 
 export const config = Object.freeze(_config);
---
+++ src/config/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { config } from "./config";
+
+const databaseUrl: string = config.databaseUrl;
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Connected to Database");
+    });
+
+    mongoose.connection.on("error", (error) => {
+      console.error("db.ts :: Error in Connection to Database : ", error);
+    });
+
+    await mongoose.connect(databaseUrl);
+  } catch (error) {
+    console.error(`db.ts :: Failed to Connect to Database: ${error}`);
+    process.exit(1); // if by any chance, the connection not happen, then what is the purpose of having server started..!
+  }
+};
+
+export default connectDB;
