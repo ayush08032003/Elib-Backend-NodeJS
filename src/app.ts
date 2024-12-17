47279f416556b301d230cc254e5266ff372471f1
AyushKumar <ayushkumar08032003@gmail.com>
2024-12-17 20:37:31 +0530

    Add Global Error Handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import globalErrorHandler from "./middlwares/globalErrorHandler";
 
 const app = express();
 
 // Routes..
 // HTTP MEHTODS: GET,  POST, PUT, DELETE, PATCH etc..
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.json({ message: "Hello World" });
+    // const error = createHttpError(404, "Not Found");
+    // throw error;
+  } catch (error) {
+    next(error);
+  }
 });
+
+// Global Error Handler.
+app.use(globalErrorHandler);
 
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,7 @@ conf();
 const _config = {
   port: process.env.PORT || 3000,
   databaseUrl: process.env.DATABASE_URL || "",
+  node_env: process.env.NODE_ENV || "production",
 };
 
 export const config = Object.freeze(_config);
