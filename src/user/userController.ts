f6af54b33e4b7be8ed3f1fa60b0f1f9833242add
AyushKumar <ayushkumar08032003@gmail.com>
2024-12-18 19:49:23 +0530

    Add LoginUser Route

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import userModel from "./userModel";
 import { config } from "../config/config";
+import { rmSync } from "fs";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -16,10 +17,17 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 
   // Database Call.
-  const user = await userModel.findOne({ email });
-  if (user !== null) {
-    const error = createHttpError(400, "User Already Exists..");
-    return next(error);
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (user !== null) {
+      const error = createHttpError(400, "User Already Exists..");
+      return next(error);
+    }
+  } catch (err) {
+    return next(
+      createHttpError(500, "createUser :: Error While Finding User in Database")
+    );
   }
 
   // Hash Password
@@ -37,7 +45,48 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     expiresIn: "7d",
   });
 
-  res.json({ message: "User Created", accessToken: token });
+  res.status(201).json({ message: "User Created", accessToken: token });
+  // make sure to have 201 status code as this creates a new resource.
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "All Fields are Required"));
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (user === null) {
+      next(createHttpError(404, "User Not Found"));
+      return;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return next(
+        createHttpError(
+          401,
+          "Invalid Credentials - User and Password Not Match.!"
+        )
+      );
+    }
+
+    const token = jwt.sign({ id: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+
+    res.json({ message: "Login Successfully", accessToken: token });
+    return;
+  } catch (err) {
+    next(
+      createHttpError(500, "loginUser :: Error While Finding User in Database")
+    );
+    return;
+  }
+  res.json({ message: "Login Successfully" });
+};
+
+export { createUser, loginUser };
